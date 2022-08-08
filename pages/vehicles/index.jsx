ec6a2c7ec2dd1b9303762f803757c153fbb34ca0
cchavezmx <cchavezmx@outlook.com>
2022-08-08 00:34:36 -0500

    fix eslint rules

--- pages/vehicles/index.jsx
+++ pages/vehicles/index.jsx
@@ -1,37 +1,36 @@
 import { Box } from '@mui/material'
-import CardVehicles from "../../Components/CardVehicles";
+import CardVehicles from '../../Components/CardVehicles'
 
-const Vehicles = ({ vehicles}) => {
+const Vehicles = ({ vehicles }) => {
   return (
   <>
     <Box
       sx={{
-        display: "Grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-        gap: "1rem",
-        marginTop: "2.5rem",
+        display: 'Grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+        gap: '1rem',
+        marginTop: '2.5rem'
       }}
-    
+
     >
        {
          vehicles.map(vehicle => <CardVehicles key={vehicle._id} vehicle={vehicle} />)
        }
     </Box>
     <footer style={{
-      height: "5rem",
+      height: '5rem'
     }}>
 
     </footer>
   </>
-  );
+  )
 }
 
-export async function getServerSideProps(context) {
-  const API = process.env.NEXT_PUBLIC_API;
+export async function getServerSideProps (context) {
+  const API = process.env.NEXT_PUBLIC_API
   const vehicles = await fetch(`${API}/flotilla/vehicles`)
-  .then(res => res.json())
-  .then(({ vehicles }) => vehicles)
-
+    .then(res => res.json())
+    .then(({ vehicles }) => vehicles)
 
   return {
     props: {
@@ -40,4 +39,4 @@ export async function getServerSideProps(context) {
   }
 }
 
-export default Vehicles;
+export default Vehicles
